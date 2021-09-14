e88dde8311104b2e7d48888a271138a17c70c60b
Mushfiqur Rahman <rahman.mushfiq2000@gmail.com>
2021-09-14 16:01:46 +0600

    changed the layout and appearance of home02 a little bit and shifted the under construction page to home03

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,11 +1,12 @@
-import Home01 from './Home01';
-import Home02 from './Home02';
+import Home01 from "./Home01";
+import Home02 from "./Home02";
+import Home03 from "./Home03";
 
 const routes = [
-    { path: "/", component: Home01},
-    { path: "/home-01", component: Home01},
-    { path: "/home-02", component: Home02},
-    // { path: "/home-03", component: Home03},
-]
+  { path: "/", component: Home01 },
+  { path: "/home-01", component: Home01 },
+  { path: "/home-02", component: Home02 },
+  { path: "/home-03", component: Home03 },
+];
 
 export default routes;
